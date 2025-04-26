8d2914a64a42bdcb7f471f7730e57feef31cddb9
Satyam <sk.m416.6x@gmail.com>
2025-04-26 09:52:02 +0530

    change

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -38,3 +37,8 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
+// 👇 This was missing
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
